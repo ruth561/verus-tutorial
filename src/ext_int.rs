9ac9b6c4a74d992e75ffe0f7089f06de3d5a9ec6
Takumi Jin <primenumber_2_3_5@yahoo.co.jp>
2024-07-14 21:20:39 +0900

    Implemented spec_lt for ExtInt（途中）
    ExtIntにNaNはない方がいいかもしれない。

--- src/ext_int.rs
+++ src/ext_int.rs
@@ -1,3 +1,4 @@
+use std::cmp::Ordering;
 use std::ops::Add;
 use std::fmt;
 
@@ -57,6 +58,19 @@ impl ExtInt {
         }
     }
 
+    pub closed spec fn spec_lt(self, rhs: ExtInt) -> bool {
+        match (self, rhs) {
+            (ExtInt::Overflow, _) => false,
+            (_, ExtInt::Overflow) => false,
+            (ExtInt::Inf, ExtInt::Inf) => false,
+            (ExtInt::Inf, _) => false,
+            (_, ExtInt::Inf) => true,
+            (ExtInt::Int(n1), ExtInt::Int(n2)) => {
+                n1 < n2
+            }
+        }
+    }
+
 }
 
 impl Add for ExtInt {
@@ -80,6 +94,69 @@ impl Add for ExtInt {
     }
 }
 
+impl PartialEq for ExtInt {
+    fn eq(&self, other: &Self) -> bool {
+        match (*self, *other) {
+            (ExtInt::Overflow, _) => false,
+            (_, ExtInt::Overflow) => false,
+            (ExtInt::Inf, ExtInt::Inf) => true,
+            (ExtInt::Inf, _) => false,
+            (_, ExtInt::Inf) => false,
+            (ExtInt::Int(n1), ExtInt::Int(n2)) => {
+                n1.eq(&n2)
+            }
+        }
+    }
+}
+
+proof fn ext_int_cmp_inf(en1: ExtInt, en2: ExtInt)
+    requires
+        en1 == ExtInt::Inf,
+        en1 < en2,
+    // ensures
+        // en2 == ExtInt::Inf,
+{}
+
+
+impl Eq for ExtInt {}
+
+#[verifier::external_type_specification]
+pub struct ExOrdering(Ordering);
+
+// #[verifier::external]
+impl PartialOrd for ExtInt {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        match (*self, *other) {
+            (ExtInt::Overflow, _) => None,
+            (_, ExtInt::Overflow) => None,
+            (ExtInt::Inf, ExtInt::Inf) => Some(Ordering::Equal),
+            (ExtInt::Inf, _) => Some(Ordering::Greater),
+            (_, ExtInt::Inf) => Some(Ordering::Less),
+            (ExtInt::Int(n1), ExtInt::Int(n2)) => {
+                if n1 == n2 {
+                    Some(Ordering::Equal)
+                } else if n1 < n2 {
+                    Some(Ordering::Less)
+                } else {
+                    Some(Ordering::Greater)
+                }
+            }
+        }
+    }
+}
+
+impl Ord for ExtInt {
+    fn cmp(&self, other: &Self) -> Ordering {
+        Ordering::Equal
+    }
+}
+
+// impl core::cmp::Ord for int {
+//     fn cmp(&self, _other: &Self) -> core::cmp::Ordering {
+//         unimplemented!()
+//     }
+// }
+
 // == i64 methods ==
 // This implementations refer to the implementations at the following URL
 // https://github.com/verus-lang/verus/blob/23a5b86e270939935df5997f9d4c8b9fcb5fddda/source/vstd/std_specs/num.rs#L244-L364
@@ -95,6 +172,17 @@ pub fn ex_i64_checked_add(lhs: i64, rhs: i64) -> (result: Option<i64>)
     lhs.checked_add(rhs)
 }
 
+proof fn trait_test()
+{
+    let a = ExtInt::Inf;
+    let b = ExtInt::Int(1000);
+    assert(a.spec_add(b) == ExtInt::Inf);
+    assert(b.spec_add(b) == ExtInt::Int(2000));
+    assert(b + b == ExtInt::Int(2000));
+
+    assert(b < a);
+}
+
 #[verifier::external_body]
 pub fn test() {
     let inf = ExtInt::Inf;
@@ -106,21 +194,34 @@ pub fn test() {
     x = inf;
     y = inf;
     println!("{} + {} = {}", x, y, x + y);
+    println!("{} < {} = {}", x, y, x < y);
 
     x = inf;
     y = n;
     println!("{} + {} = {}", x, y, x + y);
+    println!("{} < {} = {}", x, y, x < y);
 
     x = n;
     y = inf;
     println!("{} + {} = {}", x, y, x + y);
+    println!("{} < {} = {}", x, y, x < y);
 
     x = n;
     y = n;
     println!("{} + {} = {}", x, y, x + y);
+    println!("{} < {} = {}", x, y, x < y);
 
     x = ExtInt::Int(i64::MAX);
     println!("{} + {} = {}", x, x, x + x);
+    println!("{} < {} = {}", x, y, x < y);
+
+    let a = ExtInt::Inf;
+    let b = ExtInt::Int(1000);
+    if a < b {
+        println!("a is less than b");
+    } else {
+        println!("a is not less than b");
+    }
 }
 
 } // verus!
