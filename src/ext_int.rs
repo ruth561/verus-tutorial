f1e2582cc0e814e7f12bf769f9c91fd41df51dca
Takumi Jin <primenumber_2_3_5@yahoo.co.jp>
2024-07-14 22:13:19 +0900

    Splitted the definition of ExtInt into ExtInt and ResultExtInt

--- src/ext_int.rs
+++ src/ext_int.rs
@@ -12,24 +12,23 @@ verus! {
 // 対応したい.
 
 #[derive(Debug, Copy, Clone)]
-enum ExtInt {
+pub enum ExtInt {
     Int(i64),
     Inf,
-    Overflow, // overflowed
+}
+
+#[derive(Debug, Copy, Clone)]
+pub enum ResultExtInt {
+    Ok(ExtInt),
+    Overflow,
 }
 
 impl ExtInt {
-
-    pub closed spec fn is_overflow(self) -> bool {
-        self == ExtInt::Overflow
-    }
 
     // self + rhs がオーバーフローを引き起こすときはtrueを返す
     // return true iff lhs + rhs cause overflow
     pub closed spec fn spec_check_add_overflow(self, rhs: ExtInt) -> bool {
         match (self, rhs) {
-            (ExtInt::Overflow, _) => true,
-            (_, ExtInt::Overflow) => true,
             (ExtInt::Inf, _) => false,
             (_, ExtInt::Inf) => false,
             (ExtInt::Int(n1), ExtInt::Int(n2)) => {
@@ -42,26 +41,31 @@ impl ExtInt {
         }
     }
 
-    pub closed spec fn spec_add(self, rhs: ExtInt) -> ExtInt {
+    pub closed spec fn spec_checked_add(self, rhs: ExtInt) -> Option<ExtInt>
+        recommends
+            !self.spec_check_add_overflow(rhs)
+    {
         match (self, rhs) {
-            (ExtInt::Overflow, _) => ExtInt::Overflow,
-            (_, ExtInt::Overflow) => ExtInt::Overflow,
-            (ExtInt::Inf, _) => ExtInt::Inf,
-            (_, ExtInt::Inf) => ExtInt::Inf,
+            (ExtInt::Inf, _) => Some(ExtInt::Inf),
+            (_, ExtInt::Inf) => Some(ExtInt::Inf),
             (ExtInt::Int(n1), ExtInt::Int(n2)) => {
                 if i64::MIN <= n1 + n2 <= i64::MAX {
-                    ExtInt::Int((n1 + n2) as i64)
+                    Some(ExtInt::Int((n1 + n2) as i64))
                 } else {
-                    ExtInt::Overflow
+                    None
                 }
             }
         }
     }
 
+    pub proof fn lem_checked_add(self, rhs: ExtInt)
+        ensures
+            self == ExtInt::Inf || rhs == ExtInt::Inf <==> self.spec_checked_add(rhs) == Some(ExtInt::Inf),
+            self.spec_check_add_overflow(rhs) <==> self.spec_checked_add(rhs) == None::<ExtInt>,
+    {}
+
     pub closed spec fn spec_lt(self, rhs: ExtInt) -> bool {
         match (self, rhs) {
-            (ExtInt::Overflow, _) => false,
-            (_, ExtInt::Overflow) => false,
             (ExtInt::Inf, ExtInt::Inf) => false,
             (ExtInt::Inf, _) => false,
             (_, ExtInt::Inf) => true,
@@ -71,25 +75,41 @@ impl ExtInt {
         }
     }
 
+    pub closed spec fn spec_le(self, rhs: ExtInt) -> bool {
+        self.spec_lt(rhs) || self == rhs
+    }
+
+    pub closed spec fn spec_gt(self, rhs: ExtInt) -> bool {
+        !self.spec_le(rhs)
+    }
+
+    pub closed spec fn spec_ge(self, rhs: ExtInt) -> bool {
+        !self.spec_lt(rhs)
+    }
+
+    // オーバーフローを検査する加算
+    pub fn checked_add(self, rhs: ExtInt) -> (result: Option<ExtInt>)
+        ensures
+            result == self.spec_checked_add(rhs),
+            self.spec_check_add_overflow(rhs) <==> result == None::<ExtInt>,
+    {
+        match (self, rhs) {
+            (ExtInt::Inf, _) => Some(ExtInt::Inf),
+            (_, ExtInt::Inf) => Some(ExtInt::Inf),
+            (ExtInt::Int(n1), ExtInt::Int(n2)) => {
+                Some(ExtInt::Int(ex_i64_checked_add(n1, n2)?))
+            }
+        }
+    }
+
 }
 
 impl Add for ExtInt {
-    type Output = ExtInt;
-    fn add(self, rhs: Self) -> (result: Self::Output)
-        ensures
-            result == self.spec_add(rhs),
-    {
-        match (self, rhs) {
-            (ExtInt::Overflow, _) => ExtInt::Overflow,
-            (_, ExtInt::Overflow) => ExtInt::Overflow,
-            (ExtInt::Inf, _) => ExtInt::Inf,
-            (_, ExtInt::Inf) => ExtInt::Inf,
-            (ExtInt::Int(n1), ExtInt::Int(n2)) => {
-                match ex_i64_checked_add(n1, n2) {
-                    Some(n) => ExtInt::Int(n),
-                    None => ExtInt::Overflow,
-                }
-            }
+    type Output = ResultExtInt;
+    fn add(self, rhs: Self) -> Self::Output {
+        match self.checked_add(rhs) {
+            Some(n) => ResultExtInt::Ok(n),
+            None => ResultExtInt::Overflow,
         }
     }
 }
@@ -97,65 +117,48 @@ impl Add for ExtInt {
 impl PartialEq for ExtInt {
     fn eq(&self, other: &Self) -> bool {
         match (*self, *other) {
-            (ExtInt::Overflow, _) => false,
-            (_, ExtInt::Overflow) => false,
             (ExtInt::Inf, ExtInt::Inf) => true,
             (ExtInt::Inf, _) => false,
             (_, ExtInt::Inf) => false,
             (ExtInt::Int(n1), ExtInt::Int(n2)) => {
-                n1.eq(&n2)
+                n1 == n2
             }
         }
     }
 }
 
-proof fn ext_int_cmp_inf(en1: ExtInt, en2: ExtInt)
-    requires
-        en1 == ExtInt::Inf,
-        en1 < en2,
-    // ensures
-        // en2 == ExtInt::Inf,
-{}
-
-
 impl Eq for ExtInt {}
 
+// VerusのソースにもExOrderingが定義されているが、そのExOrderingは
+// core::sync::atomic::Orderingの方を言及しており、std::cmp::Ordering
+// の方ではないため、ここで定義する必要がある。
 #[verifier::external_type_specification]
 pub struct ExOrdering(Ordering);
 
-// #[verifier::external]
 impl PartialOrd for ExtInt {
     fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        match (*self, *other) {
-            (ExtInt::Overflow, _) => None,
-            (_, ExtInt::Overflow) => None,
-            (ExtInt::Inf, ExtInt::Inf) => Some(Ordering::Equal),
-            (ExtInt::Inf, _) => Some(Ordering::Greater),
-            (_, ExtInt::Inf) => Some(Ordering::Less),
-            (ExtInt::Int(n1), ExtInt::Int(n2)) => {
-                if n1 == n2 {
-                    Some(Ordering::Equal)
-                } else if n1 < n2 {
-                    Some(Ordering::Less)
-                } else {
-                    Some(Ordering::Greater)
-                }
-            }
-        }
+        Some(self.cmp(other))
     }
 }
 
 impl Ord for ExtInt {
     fn cmp(&self, other: &Self) -> Ordering {
-        Ordering::Equal
+        match (*self, *other) {
+            (ExtInt::Inf, ExtInt::Inf) => Ordering::Equal,
+            (ExtInt::Inf, _) => Ordering::Greater,
+            (_, ExtInt::Inf) => Ordering::Less,
+            (ExtInt::Int(n1), ExtInt::Int(n2)) => {
+                if n1 == n2 {
+                    Ordering::Equal
+                } else if n1 < n2 {
+                    Ordering::Less
+                } else {
+                    Ordering::Greater
+                }
+            }
+        }
     }
 }
-
-// impl core::cmp::Ord for int {
-//     fn cmp(&self, _other: &Self) -> core::cmp::Ordering {
-//         unimplemented!()
-//     }
-// }
 
 // == i64 methods ==
 // This implementations refer to the implementations at the following URL
@@ -172,13 +175,18 @@ pub fn ex_i64_checked_add(lhs: i64, rhs: i64) -> (result: Option<i64>)
     lhs.checked_add(rhs)
 }
 
-proof fn trait_test()
+proof fn test_spec()
 {
     let a = ExtInt::Inf;
     let b = ExtInt::Int(1000);
-    assert(a.spec_add(b) == ExtInt::Inf);
-    assert(b.spec_add(b) == ExtInt::Int(2000));
-    assert(b + b == ExtInt::Int(2000));
+    assert(a.spec_checked_add(b) == Some(ExtInt::Inf));
+    assert(b.spec_checked_add(b) == Some(ExtInt::Int(2000)));
+    assert(b.spec_checked_add(b) == Some(ExtInt::Int(2000)));
+
+    assert(ExtInt::Int(0) < ExtInt::Inf);
+    assert(ExtInt::Int(0) < ExtInt::Int(1));
+    assert(ExtInt::Int(1) <= ExtInt::Int(1));
+    assert(ExtInt::Inf <= ExtInt::Inf);
 
     assert(b < a);
 }
@@ -193,27 +201,27 @@ pub fn test() {
 
     x = inf;
     y = inf;
-    println!("{} + {} = {}", x, y, x + y);
-    println!("{} < {} = {}", x, y, x < y);
+    println!("{:?} + {:?} = {:?}", x, y, x + y);
+    println!("{:?} < {:?} = {:?}", x, y, x < y);
 
     x = inf;
     y = n;
-    println!("{} + {} = {}", x, y, x + y);
-    println!("{} < {} = {}", x, y, x < y);
+    println!("{:?} + {:?} = {:?}", x, y, x + y);
+    println!("{:?} < {:?} = {:?}", x, y, x < y);
 
     x = n;
     y = inf;
-    println!("{} + {} = {}", x, y, x + y);
-    println!("{} < {} = {}", x, y, x < y);
+    println!("{:?} + {:?} = {:?}", x, y, x + y);
+    println!("{:?} < {:?} = {:?}", x, y, x < y);
 
     x = n;
     y = n;
-    println!("{} + {} = {}", x, y, x + y);
-    println!("{} < {} = {}", x, y, x < y);
+    println!("{:?} + {:?} = {:?}", x, y, x + y);
+    println!("{:?} < {:?} = {:?}", x, y, x < y);
 
     x = ExtInt::Int(i64::MAX);
-    println!("{} + {} = {}", x, x, x + x);
-    println!("{} < {} = {}", x, y, x < y);
+    println!("{:?} + {:?} = {:?}", x, x, x + x);
+    println!("{:?} < {:?} = {:?}", x, y, x < y);
 
     let a = ExtInt::Inf;
     let b = ExtInt::Int(1000);
@@ -231,7 +239,6 @@ impl fmt::Display for ExtInt {
         match self {
             ExtInt::Int(n) => write!(f, "{}", *n),
             ExtInt::Inf => write!(f, "∞"),
-            ExtInt::Overflow => write!(f, "Overflow"),
         }
     }
 }
