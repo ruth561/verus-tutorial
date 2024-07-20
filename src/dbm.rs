88e8bc4a88e0ea65228d4209a23ed3d0966a1f50
Takumi Jin <primenumber_2_3_5@yahoo.co.jp>
2024-07-20 17:33:06 +0900

    Implemented and Verifies reset operation in DBM

--- src/dbm.rs
+++ src/dbm.rs
@@ -1,46 +1,64 @@
 use vstd::prelude::*;
 use vstd::seq::*;
 
-use crate::ext_i64::*;
+use crate::ext_int::*;
 
 
 verus! {
 
 struct Dbm {
     dimension: nat,
-    matrix: Seq<Seq<ExtI64>>,
+    matrix: Seq<Seq<ExtInt>>,
 }
 
 impl Dbm {
 
-    spec fn is_consistent(self) -> bool {
+    spec fn idx(self, i: int) -> bool {
+        &&& 0 <= i < self.dimension
+    }
+
+    spec fn idx2(self, i: int, j: int) -> bool {
+        &&& 0 <= i < self.dimension
+        &&& 0 <= j < self.dimension
+    }
+
+    spec fn idx3(self, i: int, j: int, k: int) -> bool {
+        &&& 0 <= i < self.dimension
+        &&& 0 <= j < self.dimension
+        &&& 0 <= k < self.dimension
+    }
+
+    spec fn triangle_inequality(self, i: int, j: int, k: int) -> bool {
+        self.matrix[i][j] <= self.matrix[i][k] + self.matrix[k][j]
+    }
+
+    spec fn well_formed(self) -> bool {
         &&& 2 <= self.dimension
         &&& is_square_matrix(self.matrix, self.dimension)
-        &&& forall|i:int| 0 <= i < self.dimension ==> #[trigger] is_zero(self.matrix[i][i])
-        &&& forall|i:int| 0 <= i < self.dimension ==> #[trigger] is_non_negative(self.matrix[i][0])
-        &&& forall|i:int| 0 <= i < self.dimension ==> #[trigger] is_non_positive(self.matrix[0][i])
+        &&& forall|i:int| self.idx(i) ==> #[trigger] self.matrix[i][i].is_zero()
+        &&& forall|i:int| self.idx(i) ==> #[trigger] self.matrix[i][0].is_non_negative()
+        &&& forall|i:int| self.idx(i) ==> #[trigger] self.matrix[0][i].is_non_positive()
     }
 
     spec fn is_canonical(self) -> bool {
-        &&& self.is_consistent()
-        &&& forall|i:int, j:int, k:int|
-            0 <= i < self.dimension &&
-            0 <= j < self.dimension &&
-            0 <= k < self.dimension ==>
-            #[trigger] triangle_inequality(self.matrix, i, j, k)
+        &&& self.well_formed()
+        &&& forall|i:int, j:int, k:int| self.idx3(i, j, k) ==> #[trigger] self.triangle_inequality(i, j, k)
     }
 
 }
 
 impl Dbm {
-    proof fn proof_reset(self, x: int, m: ExtI64) -> (result: Dbm)
+
+    // proof that the reset operation preserve the canonicity
+    proof fn proof_reset(self, x: int, m: ExtInt) -> (result: Dbm)
         requires
             self.is_canonical(),
             1 <= x < self.dimension,
-            is_non_negative(m),
+            m.is_non_negative(),
             m.is_int(),
-        // ensures
-        //     result.is_canonical(),
+        ensures
+            result.matrix == spec_reset(self.matrix, self.dimension, x, m),
+            result.is_canonical(),
     {
         let result = Dbm {
             dimension: self.dimension,
@@ -48,59 +66,41 @@ impl Dbm {
         };
         lem_reset(self.matrix, self.dimension, x, m);
 
-        // assert(is_square_matrix(result.matrix, result.dimension));
-        // assert(forall|i:int| 0 <= i < result.dimension ==> #[trigger] is_zero(result.matrix[i][i]));
-        assert(is_zero(self.matrix[0][0])); // needed for proof is_non_negative(result.matrix[x][0])
-        // assert(forall|i:int| 0 <= i < result.dimension ==> #[trigger] is_non_negative(result.matrix[i][0]));
-        assert(result.is_consistent());
+        // needed to prove
+        //   result.matrix[x][0].is_non_negative() and
+        //   result.matrix[0][x].is_non_positive()
+        // since
+        //   result.matrix[x][0] == self.matrix[0][0] + m and 
+        //   result.matrix[0][x] == self.matrix[0][0] - m
+        assert(self.matrix[0][0].is_zero());
 
-        assert(is_square_matrix(self.matrix, self.dimension));
-        assert(is_square_matrix(result.matrix, result.dimension));
-        assert(2 <= self.matrix.len());
-
-        assert forall|i:int, j:int, k:int|
-            0 <= i < result.dimension &&
-            0 <= j < result.dimension &&
-            0 <= k < result.dimension implies
-            #[trigger] triangle_inequality(result.matrix, i, j, k) by
-        {
-            assert(len_is_n(result.matrix[0], result.dimension));
-            assert(len_is_n(result.matrix[x], result.dimension));
-            assert(is_zero(result.matrix[x][x]));
+        assert forall|i:int, j:int, k:int| result.idx3(i, j, k) implies #[trigger] result.triangle_inequality(i, j, k) by {
+            assert(result.matrix[x][x].is_zero());
             if        i == x && j == x && k == x {
-                assert(is_zero(result.matrix[x][x]));
+                // proof by assert(self.matrix[0][0].is_zero())
             } else if i == x && j == x && k != x {
-                // result.matrix[x][x] == self.matrix[0][0] <= self.matrix[0][k] + self.matrix[k][0] ==
-                //     (self.matrix[0][k] + m) + (self.matrix[k][0] - m) == result.matrix[x][k] + result.matrix[k][x]
-                assert(is_non_positive(self.matrix[0][k])); // precondition for non-overflow `self.matrix[0][k] + m`
-                assert(result.matrix[x][k] == self.matrix[0][k] + m);
-                assert(is_non_negative(self.matrix[k][0])); // precondition for non-overflow `self.matrix[k][0] - m`
-                assert(result.matrix[k][x] == self.matrix[k][0] - m);
-                assert(self.matrix[0][k] + self.matrix[k][0] == (self.matrix[0][k] + m) + (self.matrix[k][0] - m));
-                assert(result.matrix[x][k] + result.matrix[k][x] == self.matrix[0][k] + self.matrix[k][0]);
-                assert(triangle_inequality(self.matrix, 0, 0, k));
-                assert(result.matrix[x][x] <= result.matrix[x][k] + result.matrix[k][x]);
+                // result.matrix[x][x] == self.matrix[0][0] <= self.matrix[0][k] + self.matrix[k][0]
+                //   == (self.matrix[0][k] + m) + (self.matrix[k][0] - m) == result.matrix[x][k] + result.matrix[k][x]
+                assert(self.triangle_inequality(0, 0, k));
             } else if i == x && j != x && k == x {
+                // proof by assert(self.matrix[x][x].is_zero())
             } else if i == x && j != x && k != x {
-                // result.matrix[x][j] == self.matrix[0][j] + m <= (self.matrix[0][k] + m) + self.matrix[k][j] == result.matrix[x][k] + result.matrix[k][j]
-                // assert(is_non_positive(self.matrix[0][k])); // precondition for non-overflow `self.matrix[0][k] + m`
-                // assert(result.matrix[x][k] == self.matrix[0][k] + m);
-                // assert(is_non_positive(self.matrix[0][j])); // precondition for non-overflow `self.matrix[0][k] + m`
-                // assert(result.matrix[x][j] == self.matrix[0][j] + m);
-                // assert(triangle_inequality(self.matrix, 0, j, k));
-                // assert(len_is_n(self.matrix[k], self.dimension));
-                // assert(self.matrix[0][j] + m <= (self.matrix[0][k] + self.matrix[k][j]) + m);
-                // assert(self.matrix[0][j] + m <= (self.matrix[0][k] + m) + self.matrix[k][j]);
-
-                assume(false);
-            }
-
-            else {
-                assume(false);
+                // result.matrix[x][j] == self.matrix[0][j] + m <= (self.matrix[0][k] + m) + self.matrix[k][j]
+                //   == result.matrix[x][k] + result.matrix[k][j]
+                assert(self.triangle_inequality(0, j, k));
+            } else if i != x && j == x && k == x {
+                // proof by assert(self.matrix[x][x].is_zero())
+            } else if i != x && j == x && k != x {
+                // same as above
+                assert(self.triangle_inequality(i, 0, k));
+            } else if i != x && j != x && k == x {
+                // same as above
+                assert(self.triangle_inequality(i, j, 0));
+            } else {//i != x && j != x && k != x
+                // no change
+                assert(self.triangle_inequality(i, j, k));
             }
         };
-        assert(result.is_canonical());
-
         result
     }
 }
@@ -115,71 +115,6 @@ spec fn len_is_n<T>(seq: Seq<T>, n: nat) -> bool {
     seq.len() == n
 }
 
-spec fn is_zero(ei: ExtI64) -> bool {
-    ei == ExtI64::Int(0)
-}
-
-spec fn is_non_negative(ei: ExtI64) -> bool {
-    ||| ei.is_inf()
-    ||| ei.is_int() && 0 <= ei.unwrap()
-}
-
-spec fn is_non_positive(ei: ExtI64) -> bool {
-    ei.is_int() && 0 >= ei.unwrap()
-}
-
-// 非負を足したら、結果が小さくなることはない。
-proof fn lem_add_non_negative(e1: ExtI64, e2: ExtI64)
-    requires
-        !e1.check_add_overflow(e2),
-        is_non_negative(e2),
-    ensures
-        e1 <= e1 + e2,
-{}
-
-// #[verifier::external_body]
-proof fn lem_add_plus_minus(e1: ExtI64, e2: ExtI64, m: ExtI64)
-    requires
-        !e1.check_add_overflow(e2),
-        !e1.check_add_overflow(m),
-        !e2.check_sub_overflow(m),
-    ensures
-        e1 + e2 == (e1 + m) + (e2 - m)
-{
-    // assert(m.is_int()); // from !e2.check_sub_overflow(m)
-}
-
-proof fn lem_assoc(e1: ExtI64, e2: ExtI64, e3: ExtI64)
-    requires
-        !e1.check_add_overflow(e2),
-        !e2.check_add_overflow(e3),
-        // !(e1 + e2).check_add_overflow(e3),
-        // !(e1 + e2).check_add_overflow(e3),
-    ensures
-        (e1 + e2) + e3 == e1 + (e2 + e3),
-{
-    match (e1, e2, e3) {
-        (ExtI64::Inf, _, _) => (),
-        (_, ExtI64::Inf, _) => (),
-        (_, _, ExtI64::Inf) => (),
-        (ExtI64::Int(n1), ExtI64::Int(n2), ExtI64::Int(n3)) => {
-            // assert((n1 + n2) as i64 + n3 == n1 + (n2 + n3) as i64);
-            // assert(e1 + e2 == ExtI64::Int((n1 + n2) as i64));
-            // assert((e1 + e2) + e3 == ExtI64::Int(((n1 + n2) as i64 + n3) as i64));
-
-            // assert(e1 + e2 == ExtI64::Int((n1 + n2) as i64));
-            // assert((e1 + e2) + e3 == ExtI64::Int(((n1 + n2) as i64 + n3) as i64));
-
-            // assert(e2 + e3 == ExtI64::Int((n2 + n3) as i64));
-            // assert(e1 + (e2 + e3) == ExtI64::Int((n1 + (n2 + n3) as i64) as i64));
-        },
-    }
-}
-
-spec fn triangle_inequality(matrix: Seq<Seq<ExtI64>>, i: int, j: int, k: int) -> bool {
-    matrix[i][j] <= matrix[i][k] + matrix[k][j]
-}
-
 // resetの処理の前後におけるDBMの関係性を示す述語
 // x行とx列の値が更新され、それ以外の値はd_oldのまま
 // [[---, ???, ... , ---],
@@ -188,18 +123,18 @@ spec fn triangle_inequality(matrix: Seq<Seq<ExtI64>>, i: int, j: int, k: int) ->
 //    ...
 //  [---, ???, ... , ---]]
 // 単に行列の変換処理を表現したもの
-spec fn spec_reset(matrix: Seq<Seq<ExtI64>>, n: nat, x: int, m: ExtI64) -> Seq<Seq<ExtI64>>
+spec fn spec_reset(matrix: Seq<Seq<ExtInt>>, n: nat, x: int, m: ExtInt) -> Seq<Seq<ExtInt>>
     recommends
-        is_square_matrix(matrix, n),
         1 <= x < n,
-        is_non_negative(m),
+        is_square_matrix(matrix, n),
+        m.is_non_negative(),
         m.is_int(),
 {
-    matrix.map(|i:int, seq:Seq<ExtI64>| {
+    matrix.map(|i:int, seq:Seq<ExtInt>| {
         if i != x {
             seq.update(x, seq[0] - m)
         } else {
-            matrix[0].map(|j:int, e:ExtI64|
+            matrix[0].map(|j:int, e:ExtInt|
                 if j != x {
                     e + m
                 } else {
@@ -210,11 +145,11 @@ spec fn spec_reset(matrix: Seq<Seq<ExtI64>>, n: nat, x: int, m: ExtI64) -> Seq<S
     })
 }
 
-proof fn lem_reset(matrix: Seq<Seq<ExtI64>>, n: nat, x: int, m: ExtI64)
+proof fn lem_reset(matrix: Seq<Seq<ExtInt>>, n: nat, x: int, m: ExtInt)
     requires
-        is_square_matrix(matrix, n),
         1 <= x < n,
-        is_non_negative(m),
+        is_square_matrix(matrix, n),
+        m.is_non_negative(),
         m.is_int(),
     ensures
         is_square_matrix(spec_reset(matrix, n, x, m), n),
